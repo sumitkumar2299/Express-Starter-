43f711e43a2ffa94e20ea0a897d2b72746f2f832
sumitkumar2299 <sumitofficialme@gmail.com>
2025-03-28 17:55:08 +0530

    resolve image issue

--- src/Services/ProductService.js
+++ src/Services/ProductService.js
@@ -61,7 +61,8 @@ const NotFoundError = require('../utils/notFoundError');
          try {
              const cloudinaryResponse = await cloudinary.uploader.upload(imagePath);
              var productImage = cloudinaryResponse.secure_url;
-             await fs.unlink(imagePath);
+            //  await fs.unlink(imagePath);
+            await fs.unlink(process.cwd() + "/" + imagePath);
          } catch(error) {
              console.log(error);
              throw new internalServerError();
